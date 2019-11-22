d2ff5933c49be2cfe37cbd6c99329a8819d1f542
Nico Del Rosario <57257533+Nirioppai@users.noreply.github.com>
2019-11-22 09:34:32 -0800

    Updated App Name, Timezone

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('PSI Queue Management System', 'Laravel'),
 
     /*
     |--------------------------------------------------------------------------
@@ -67,7 +67,7 @@ return [
     |
     */
 
-    'timezone' => 'UTC',
+    'timezone' => 'Asia/Singapore',
 
     /*
     |--------------------------------------------------------------------------
