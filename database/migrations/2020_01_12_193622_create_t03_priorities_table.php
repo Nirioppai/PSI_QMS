7d20dd0d7921f5a4fae63daa7b0dd15e38af5a31
Freshmaker-L <48955345+FreshMaker-L@users.noreply.github.com>
2020-02-01 08:50:18 +0800

    Create 2020_01_12_193622_create_t03_priorities_table.php

---
+++ database/migrations/2020_01_12_193622_create_t03_priorities_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateT03PrioritiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement("CREATE VIEW t03_priorities AS 
+                        SELECT 
+                                id,
+                                user_id,
+                                queue_name,
+                                queue_number,
+                                client_name,
+                                queue_station_number,
+                                queue_window_number,
+                                queue_action,
+                                queue_priority,
+                                queue_note,
+                                created_at
+                        FROM
+                             pools
+                        WHERE 
+                        (     queue_action = '0'
+                        OR
+                            queue_action = '3' )
+                        AND 
+                            queue_priority = '1'
+
+                        ");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('t03_priorities');
+    }
+}
