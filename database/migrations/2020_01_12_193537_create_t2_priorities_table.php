0ec010f568badb764e7715e22c2f462615f888c2
Freshmaker-L <48955345+FreshMaker-L@users.noreply.github.com>
2020-02-01 08:49:49 +0800

    Create 2020_01_12_193537_create_t2_priorities_table.php

---
+++ database/migrations/2020_01_12_193537_create_t2_priorities_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateT2PrioritiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement("CREATE VIEW t2_priorities AS 
+                        SELECT 
+                                id,
+                                user_id,
+                                queue_name,
+                                queue_number,
+                                client_name,
+                                queue_station_number,
+                                queue_window_number,
+                                queue_action,
+                                queue_priority,
+                                queue_note,
+                                created_at
+                        FROM
+                             pools
+                        WHERE 
+                             queue_action = '2'
+                        AND 
+                            queue_priority = '1'
+                        ");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('t2_priorities');
+    }
+}
